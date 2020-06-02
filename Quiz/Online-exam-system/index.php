ce8e4742e63be0249d007077a1d55c4b78baa790
bobermanBR <47926534+bobermanBR@users.noreply.github.com>
2020-06-02 12:19:48 -0300

    Atualizada ae meo

---
+++ Quiz/Online-exam-system/index.php
@@ -0,0 +1,559 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Onine examiner</title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<?php if(@$_GET['w'])
+{echo'<script>alert("'.@$_GET['w'].'");</script>';}
+?>
+
+
+
+
+  
+  <style>
+
+#img1{
+
+}
+.modal {
+  text-align: center;
+}
+
+@media screen and (min-width: 768px) { 
+  .modal:before {
+    display: inline-block;
+    vertical-align: middle;
+    content: " ";
+    height: 100%;
+  }
+}
+
+.modal-dialog {
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
+}
+
+
+
+  {
+      font: 400 15px Lato, sans-serif;
+      line-height: 1.8;
+      color: #818181;
+  }
+  h2 {
+      font-size: 24px;
+      text-transform: uppercase;
+      color: #303030;
+      font-weight: 600;
+      margin-bottom: 30px;
+  }
+  h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      color: #303030;
+      font-weight: 400;
+      margin-bottom: 30px;
+  }  
+  .left{
+  float:left;
+}
+
+.right{
+  float:right;
+}
+
+.clear{
+  clear:both;
+}
+  .jumbotron {
+      background-color: #f4511e;
+      color: #fff;
+      padding: 100px 25px;
+      font-family: Montserrat, sans-serif;
+  }
+  .container-fluid {
+      padding: 60px 50px;
+  }
+  .bg-grey {
+      background-color: #f6f6f6;
+  }
+  .logo-small {
+      color: #f4511e;
+      font-size: 50px;
+  }
+  .logo {
+      color: #f4511e;
+      font-size: 200px;
+  }
+ 
+  .thumbnail {
+      padding: 0 0 15px 0;
+      border: none;
+      border-radius: 0;
+  }
+  .thumbnail img {
+      width: 100%;
+      height: 100%;
+      margin-bottom: 10px;
+  }
+  .carousel-control.right, .carousel-control.left {
+      background-image: none;
+      color: #f4511e;
+  }
+  .carousel-indicators li {
+      border-color: #f4511e;
+  }
+  .carousel-indicators li.active {
+      background-color: #f4511e;
+  }
+  .item h4 {
+      font-size: 19px;
+      line-height: 1.375em;
+      font-weight: 400;
+      font-style: italic;
+      margin: 70px 0;
+  }
+  .item span {
+      font-style: normal;
+  }
+  .panel {
+      border: 1px solid #f4511e; 
+      border-radius:0 !important;
+      transition: box-shadow 0.5s;
+  }
+  .panel:hover {
+      box-shadow: 5px 0px 40px rgba(0,0,0, .2);
+  }
+  .panel-footer .btn:hover {
+      border: 1px solid #f4511e;
+      background-color: #fff !important;
+      color: #f4511e;
+  }
+  .panel-heading {
+      color: #fff !important;
+      background-color: #f4511e !important;
+      padding: 25px;
+      border-bottom: 1px solid transparent;
+      border-top-left-radius: 0px;
+      border-top-right-radius: 0px;
+      border-bottom-left-radius: 0px;
+      border-bottom-right-radius: 0px;
+  }
+  .panel-footer {
+      background-color: white !important;
+  }
+  .panel-footer h3 {
+      font-size: 32px;
+  }
+  .panel-footer h4 {
+      color: #aaa;
+      font-size: 14px;
+  }
+  .panel-footer .btn {
+      margin: 15px 0;
+      background-color: #f4511e;
+      color: #fff;
+  }
+  .navbar {
+      margin-bottom: 0;
+
+      background-color: #f4511e;
+      z-index: 9999;
+      border: 0;
+      
+      font-size: 12px !important;
+      line-height: 1.42857143 !important;
+      letter-spacing: 4px;
+      border-radius: 0;
+      font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+      color: #f4511e !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+      color: #f4511e !important;
+      background-color: #fff !important;
+
+  }
+  .navbar-default .navbar-toggle {
+      border-color: transparent;
+      color: #fff !important;
+  }
+  footer .glyphicon {
+      font-size: 20px;
+      margin-bottom: 20px;
+      color: #f4511e;
+  }
+  .slideanim {visibility:hidden;}
+  .slide {
+      animation-name: slide;
+      -webkit-animation-name: slide;
+      animation-duration: 1s;
+      -webkit-animation-duration: 1s;
+      visibility: visible;
+  }
+  @keyframes slide {
+    0% {
+      opacity: 0;
+      transform: translateY(70%);
+    } 
+    100% {
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+  @-webkit-keyframes slide {
+    0% {
+      opacity: 0;
+      -webkit-transform: translateY(70%);
+    } 
+    100% {
+      opacity: 1;
+      -webkit-transform: translateY(0%);
+    }
+  }
+  @media screen and (max-width: 768px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+    .btn-lg {
+        width: 100%;
+        margin-bottom: 35px;
+    }
+  }
+  @media screen and (max-width: 480px) {
+    .logo {
+        font-size: 150px;
+    }
+  }
+  </style>
+
+
+</head>
+
+
+
+
+
+<body>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage"><span class="glyphicon glyphicon-home"></span></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#" data-toggle="modal" data-target="#login">ADMIN</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#myModal">ENTRAR</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#myModal1">CADASTRAR-SE</a></li>
+        <li><a href="#" data-toggle="modal" data-target="#login2">PROFESSOR</a></li>
+        <li><a href="#services" >PROPÓSITO</a></li>
+        <li><a href="#about">SOBRE</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron text-center">
+  <h1>QUIZ ONLINE</h1> 
+  <p>EXAMES</p> 
+  <!--  form>
+    <div class="input-group">
+      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Subscribe</button>
+      </div>
+    </div>
+  </form -->
+</div>
+
+
+
+<!-- Container (Admin Section) -->
+<div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button>
+        <h4 class="modal-title"><span style="color:#f4511e;font-family:Montserrat, sans-serif; font-size: 20px !important;letter-spacing: 4px; "><b>ENTRAR -ADMIN</b></span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="head.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Nome de usuário" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Senha" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Entrar" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+<!----Teacher signin--->
+<div class="modal fade" id="login2">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button>
+        <h4 class="modal-title"><span style="color:#f4511e;font-family:Montserrat, sans-serif; font-size: 20px !important;letter-spacing: 4px; "><b>ENTRAR -PROFESSOR(A)</b></span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Nome de usuário" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Senha" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login2" value="Entrar" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+<div class="container-fluid bg-grey">
+  <div class="row">
+    <div class="col-sm-4">
+ <img src="globo.png" width="300" height="300">
+    </div>
+    <div class="col-sm-8">
+      <h2>Nossos Valores</h2><br>
+      <h4><strong>MISSÃO:</strong> Nossa missão é utilizar essa plataforma para poder fazer questionários online. Diminuindo o uso de papel e oferecendo resultados instantâneos. </h4><br>
+      <p><strong>O QUE VISAMOS:</strong> Nossa visão do Quiz Online é usar a tecnologia atual no sistema de exames. Pode ser útil para resultados rápidos e precisos.</p>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Container (USERS section) -->
+<!--sign in modal start-->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:#f4511e;font-family:Montserrat, sans-serif; font-size: 20px !important;letter-spacing: 4px; "><b>ENTRAR -USUÁRIO</b></span></h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" action="login.php?q=index.php" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="email"></label>  
+  <div class="col-md-6">
+  <input id="email" name="email" placeholder="Coloque seu Email" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Coloque sua senha" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-primary">Entrar</button>
+    </fieldset>
+</form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--sign in modal closed-->
+
+<!--sign up modal start-->
+<div class="modal fade" id="myModal1">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span  style="color:#f4511e;font-family:Montserrat, sans-serif; font-size: 20px !important;letter-spacing: 4px; "><b>Criar Conta</b></span></h4>
+      </div>
+      <div class="modal-body">
+       <form class="form-horizontal" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="name"></label>  
+  <div class="col-md-6">
+  <input id="name" name="name" placeholder="Coloque seu nome" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="gender"></label>
+  <div class="col-md-6">
+    <select id="gender" name="gender" placeholder="Coloque seu gênero" class="form-control input-md" >
+   <option value="Male">Selecione o Gênero</option>
+  <option value="M">Masculino</option>
+  <option value="F">Feminino</option> </select>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="name"></label>  
+  <div class="col-md-6">
+  <input id="college" name="college" placeholder="Coloque o nome da sua escola" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label title1" for="email"></label>
+  <div class="col-md-6">
+    <input id="email" name="email" placeholder="Coloque o seu Email" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="mob"></label>  
+  <div class="col-md-6">
+  <input id="mob" name="mob" placeholder="Coloque seu número de celular" class="form-control input-md" type="number">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Coloque sua senha" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-3 control-label" for="cpassword"></label>
+  <div class="col-md-6">
+    <input id="cpassword" name="cpassword" placeholder="Confirme sua senha" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+<?php if(@$_GET['q7'])
+{ echo'<p style="color:red;font-size:15px;">'.@$_GET['q7'];}?>
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-3 control-label" for=""></label>
+  <div class="col-md-6"> 
+    <input  type="submit"  style="background: #f4511e;" class="sub" value="Criar Conta"  />
+  </div>
+</div>
+</fieldset>
+</form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--sign up modal closed-->
+
+
+
+
+
+
+
+
+<!-- Container (Services Section) -->
+<div id="services" class="container-fluid text-center">
+  <h2>Nosso propósito</h2>
+  <h4>Trazer a todos, uma maneira simples de promover exames online para todos, ajudando profressores e alunos de todas as escolas.</h4>
+  <img src="photo.png" height="650" width="1200">
+  <br> 
+  </div>
+  <br><br>
+
+
+
+
+<!-- Container (About Section) -->
+<div id="about" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Sobre a Página</h2><br>
+      <h4>Sistema está realizando um teste on-line para medir o conhecimento dos participantes sobre um determinado tópico. Antigamente, todos tinham que se reunir na sala de aula ao mesmo tempo para fazer um exame. Com o exame on-line, os alunos podem fazer o exame on-line, em seu próprio tempo e com seu próprio dispositivo, independentemente de onde residem. Você on-line precisa de um navegador e de uma conexão à Internet. </h4><br>
+      
+
+
+
+
+
+    
+</footer>
+
+</script>
+
+</body>
+
+
+</html>
+
+
+
+
+
